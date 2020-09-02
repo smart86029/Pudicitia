a5ddf21a75f6a6e83d41aec590188de7c510e7de
Bill Hsu <smart86029@gmail.com>
2020-09-02 16:19:08 +0800

    feat(hr): add organization service

---
+++ src/Pudicitia.HR.Api/Services/OrganizationService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Pudicitia.Hr;
+using Pudicitia.HR.App.Organization;
+
+namespace Pudicitia.HR.Api
+{
+    public class OrganizationService : Organization.OrganizationBase
+    {
+        private readonly ILogger<OrganizationService> logger;
+        private readonly OrganizationApp organizationApp;
+
+        public OrganizationService(
+            ILogger<OrganizationService> logger,
+            OrganizationApp organizationApp)
+        {
+            this.logger = logger;
+            this.organizationApp = organizationApp;
+        }
+
+        public override async Task<ListDepartmentsResponse> ListDepartments(ListDepartmentsRequest request, ServerCallContext context)
+        {
+            var departments = await organizationApp.GetDepartmentsAsync();
+            var items = departments.Select(x => new Department
+            {
+                Id = x.Id.ToString(),
+                Name = x.Name,
+                Parentid = x.ParentId?.ToString(),
+            });
+            var result = new ListDepartmentsResponse();
+            result.Items.AddRange(items);
+
+            return result;
+        }
+
+        public override Task<Empty> CreateDepartment(CreateDepartmentRequest request, ServerCallContext context)
+        {
+            return base.CreateDepartment(request, context);
+        }
+    }
+}
--- src/Pudicitia.HR.Api/Startup.cs
+++ src/Pudicitia.HR.Api/Startup.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pudicitia.HR.Data;
 
 namespace Pudicitia.HR.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            services.AddDbContext<HRContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Database")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,7 +36,7 @@ namespace Pudicitia.HR.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<OrganizationService>();
 
                 endpoints.MapGet("/", async context =>
                 {
