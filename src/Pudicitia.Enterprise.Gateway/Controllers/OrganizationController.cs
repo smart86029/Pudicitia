705b772191a169352bfea5d032bc5a9713ebf8ed
Bill Hsu <smart86029@gmail.com>
2020-09-03 17:57:39 +0800

    feat(enterprise): add gateway project

---
+++ src/Pudicitia.Enterprise.Gateway/Controllers/OrganizationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pudicitia.Enterprise.Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationController : ControllerBase
+    {
+        private readonly ILogger<OrganizationController> logger;
+        private readonly Organization.OrganizationClient organizationClient;
+
+        public OrganizationController(
+            ILogger<OrganizationController> logger,
+            Organization.OrganizationClient organizationClient)
+        {
+            this.logger = logger;
+            this.organizationClient = organizationClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var result = organizationClient.ListDepartments(new ListDepartmentsRequest());
+
+            await Task.CompletedTask;
+            return Ok(result);
+        }
+    }
+}
---
+++ src/Pudicitia.Enterprise.Gateway/Startup.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Pudicitia.Enterprise.Gateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddTransient<A>();
+
+            services
+                .AddGrpcClient<Organization.OrganizationClient>(x => x.Address = new Uri(Configuration["Apis:HR"]))
+                .AddInterceptor<A>()
+                .ConfigurePrimaryHttpMessageHandler(() => GetClientHandler());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private HttpClientHandler GetClientHandler()
+        {
+            var result = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+            };
+
+            return result;
+        }
+    }
+
+    public class A : Interceptor
+    {
+        private ILogger<A> logger;
+
+        public A(ILogger<A> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        public override TResponse BlockingUnaryCall<TRequest, TResponse>(
+            TRequest request, ClientInterceptorContext<TRequest, TResponse> context, BlockingUnaryCallContinuation<TRequest, TResponse> continuation)
+        {
+            try
+            {
+                //return base.AsyncUnaryCall(request, context, continuation);
+                return continuation(request, context);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "123");
+                throw new RpcException(new Status(StatusCode.Unknown, e.ToString()));
+            }
+        }
+
+        public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
+            TRequest request, ClientInterceptorContext<TRequest, TResponse> context, AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
+        {
+
+            try
+            {
+                //return base.AsyncUnaryCall(request, context, continuation);
+                return continuation(request, context);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "123");
+                throw new RpcException(new Status(StatusCode.Unknown, "A"));
+            }
+        }
+    }
+}
