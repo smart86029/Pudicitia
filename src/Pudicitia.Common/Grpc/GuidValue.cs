7590e26948f383a6433a309419df0408aaa2632f
Bill Hsu <smart86029@gmail.com>
2020-09-25 16:27:57 +0800

    feat(common): add guid protobuf type

---
+++ src/Pudicitia.Common/Grpc/GuidValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Pudicitia.Common.Extensions;
+
+namespace Pudicitia.Common
+{
+    public partial class GuidValue
+    {
+        public GuidValue(string value, bool hasValue)
+        {
+            Value = value ?? string.Empty;
+            HasValue = hasValue;
+        }
+
+        public static implicit operator Guid?(GuidValue guidValue)
+        {
+            return guidValue.HasValue ? guidValue.Value.ToGuid() : default(Guid?);
+        }
+
+        public static implicit operator GuidValue(Guid? guid)
+        {
+            return new GuidValue(guid?.ToString(), guid.HasValue);
+        }
+    }
+}
