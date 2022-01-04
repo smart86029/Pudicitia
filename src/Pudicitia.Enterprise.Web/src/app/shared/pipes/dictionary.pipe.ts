31f66d4569afe8d1d0b589df161cb3dfa81b401c
Bill Hsu <smart86029@gmail.com>
2022-01-04 11:58:40 +0800

    fix(enterprise): define `DictionaryPipe` value type

--- src/Pudicitia.Enterprise.Web/src/app/shared/pipes/dictionary.pipe.ts
+++ src/Pudicitia.Enterprise.Web/src/app/shared/pipes/dictionary.pipe.ts
@@ -1,25 +1,26 @@
+import { KeyValue } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'dictionary',
 })
 export class DictionaryPipe implements PipeTransform {
-  transform(value: any): any[] {
-    const result: any[] = [];
-    if (this.isEnum(value)) {
-      const keys = Object.keys(value);
+  transform<TValue>(input: { [key: string]: TValue }): KeyValue<string, TValue>[] {
+    const result: KeyValue<string, TValue>[] = [];
+    if (this.isEnum(input)) {
+      const keys = Object.keys(input);
       keys
         .slice(keys.length / 2)
-        .forEach(key => result.push({ key, value: value[key] }));
+        .forEach(key => result.push({ key, value: input[key] }));
     }
     return result;
   }
 
-  private isEnum(input: any): boolean {
+  private isEnum<TValue>(input: { [key: string]: TValue }): boolean {
     const keys = Object.keys(input);
     const values: string[] = [];
     let result = true;
-    keys.forEach(key => values.push(input[key]));
+    keys.forEach(key => values.push(String(input[key])));
     keys.forEach(key => {
       if (!values.includes(key)) {
         result = false;
